52655dac73d7ef792c3b2e740a11b20c686ce759
william wright <william86378@williams-Mac.local>
2015-02-26 15:28:53 -0800

    Initial Commit

---
+++ school checker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  school checker
+//
+//  Created by william wright  on 2/26/15.
+//  Copyright (c) 2015 A.R.C softwere and enginering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
